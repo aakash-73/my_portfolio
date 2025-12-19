e68516a1c94df3f5ad3244b162679fdbb77b8217
AakashReddy <aakashreddy3702@gmail.com>
2025-12-19 13:32:12 -0600

    setup

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my_portfolio/',
   server: {
     port: 3000,
     open: true
